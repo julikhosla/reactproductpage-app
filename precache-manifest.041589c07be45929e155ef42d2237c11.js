074924e66139460faf1bb8b4f892bd73fee0e5ed
juliruth17 <julikhosla17@gmail.com>
2020-10-16 09:23:27 +0530

    Updates

---
+++ precache-manifest.041589c07be45929e155ef42d2237c11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae74a55683e31d93f33e362fac482907",
+    "url": "/reactproductpage-app/index.html"
+  },
+  {
+    "revision": "f3c57f462da4587c17b1",
+    "url": "/reactproductpage-app/static/css/main.82d7d1e7.chunk.css"
+  },
+  {
+    "revision": "9bd065c4ba4d9dc74952",
+    "url": "/reactproductpage-app/static/js/2.c1cf57eb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reactproductpage-app/static/js/2.c1cf57eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3c57f462da4587c17b1",
+    "url": "/reactproductpage-app/static/js/main.74306fdc.chunk.js"
+  },
+  {
+    "revision": "5dc50bad7b650d9f158b",
+    "url": "/reactproductpage-app/static/js/runtime-main.269bb670.js"
+  }
+]);
